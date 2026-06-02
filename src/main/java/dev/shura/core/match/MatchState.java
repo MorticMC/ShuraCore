46160fe366e76e135393926f30ef5ef1156a2d88
MorticMC <puremorticmc@gmail.com>
2026-06-02 20:08:19 +0500

    Delete src/main directory

--- src/main/java/dev/shura/core/match/MatchState.java
+++
@@ -1,9 +0,0 @@
-package dev.shura.core.match;
-
-public enum MatchState {
-    WAITING,
-    STARTING,
-    IN_PROGRESS,
-    ENDING,
-    FINISHED
-}
--- src/main/java/dev/shura/core/match/MatchType.java
+++
@@ -1,7 +0,0 @@
-package dev.shura.core.match;
-
-public enum MatchType {
-    RANKED,     // competitive queue, ELO affected
-    UNRANKED,   // duel request or party match, no ELO
-    PRACTICE    // FFA practice, no ELO
-}
